1577ff52d02b27657eeed30f07656936d3c23d65
Gabriel Stan <gabriel.stan.90@gmail.com>
2015-11-01 17:38:17 +0100

    testing insert tfg

--- gestion_tfg/gestion_tfg/servicios/tfg_services.py
+++ gestion_tfg/gestion_tfg/servicios/tfg_services.py
@@ -3,15 +3,41 @@ from gestion_tfg.models import Tfg
 
 def insert_tfg(tfg):
 
-    save = True
-
-
-    if (tfg.n_alumnos is None) or (not hasattr(tfg,'tutor')) or (not hasattr(tfg,'cotutor')):
-        save = False
-
-    if save:
-        print("ha pasado")
-        tfg.save()
-        return True
-    else:
+    # comprobando titulo vacio o Tfg con el mismo titulo
+    if not tfg.titulo:
         return False
+    else:
+        res = Tfg.objects.filter(titulo=tfg.titulo)
+        if res.count() != 0:
+            return False
+
+    # comprobando tipo no vacio
+    if not tfg.tipo:
+        return False
+
+    # comprobando numero de alumnos
+    if (tfg.n_alumnos is None) or (tfg.n_alumnos <= 0) or (tfg.n_alumnos > 3):
+        return False
+
+    #comprobando descripcion
+    if not tfg.descripcion:
+        return False
+
+    #comprobando conocimientos previos
+    if not tfg.conocimientos_previos:
+        return False
+
+    #comprobando requisitos HW SW
+    if not tfg.hard_soft:
+        return False
+
+    #comprobando tutor
+    if (not hasattr(tfg,'tutor')):
+        return False
+
+    #comprobando cotutor
+    if (not hasattr(tfg,'cotutor')):
+        return False
+
+    tfg.save()
+    return True
--- gestion_tfg/gestion_tfg/tests/test_tfg_services.py
+++ gestion_tfg/gestion_tfg/tests/test_tfg_services.py
@@ -10,6 +10,7 @@ class TfgServicesTests(TestCase):
 
         self.tipo_tfg = 'tipo1'
         self.titulo_tfg = 'titulo1'
+        self.otro_titulo_tfg = 'titulo2'
         self.n_alumnos_tfg = 2
         self.descripcion_tfg = 'descripcion'
         self.conocimientos_previos_tfg = 'conocimientos previos'
@@ -20,57 +21,167 @@ class TfgServicesTests(TestCase):
         self.user_cotutor_tfg = User.objects.create_user(
             username='paco', email='paco@ugr.es', password='top_secret')
 
-    def test_insert_tfg_vacio(self):
-
-        tfg1 = Tfg(tipo=self.tipo_tfg, titulo=self.titulo_tfg,
+        self.tfg1 = Tfg(tipo=self.tipo_tfg, titulo=self.titulo_tfg,
                    n_alumnos=self.n_alumnos_tfg, descripcion=self.descripcion_tfg,
                    conocimientos_previos=self.conocimientos_previos_tfg,
                    hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
                    cotutor=self.user_cotutor_tfg)
 
-        #TFG vacio
+        # insertamos un tfg valido
+        tfg_services.insert_tfg(self.tfg1)
+
+    def test_insert_tfg_vacio(self):
+
         tfg = Tfg()
         result = tfg_services.insert_tfg(tfg)
         self.assertEqual(result, False)
 
+    def test_insert_tfg_correcto(self):
+
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.otro_titulo_tfg,
+                   n_alumnos=self.n_alumnos_tfg, descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
+        result = tfg_services.insert_tfg(tfg)
+        self.assertEqual(result, True)
 
     def test_tfg_titulo(self):
-        tfg = Tfg()
+
+        #sin titulo
+        tfg = Tfg(tipo=self.tipo_tfg,
+                   n_alumnos=self.n_alumnos_tfg, descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
+
         result = tfg_services.insert_tfg(tfg)
-        self.assertEqual(True,True)
+        self.assertEqual(result, False)
+
+        #tfg con titulo duplicado - no deberia pasar
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.titulo_tfg,
+                   n_alumnos=self.n_alumnos_tfg, descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
+
+        result = tfg_services.insert_tfg(tfg)
+        self.assertEqual(result, False)
+
+        #tfg con otro titulo
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.otro_titulo_tfg,
+                   n_alumnos=self.n_alumnos_tfg, descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
+
+        result = tfg_services.insert_tfg(tfg)
+        self.assertEqual(result, True)
 
     def test_tfg_tipo(self):
-        tfg = Tfg()
+
+        #tfg sin tipo
+        tfg = Tfg(titulo=self.otro_titulo_tfg,
+                   n_alumnos=self.n_alumnos_tfg, descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
+
         result = tfg_services.insert_tfg(tfg)
-        self.assertEqual(True,True)
+        self.assertEqual(result, False)
 
     def test_tfg_n_alumnos(self):
 
-        tfg = Tfg(n_alumnos=10)
+        #tfg sin numero de alumnos
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.otro_titulo_tfg,
+                   descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
+
+        result = tfg_services.insert_tfg(tfg)
+        self.assertEqual(result, False)
+
+    def test_tfg_n_alumnos_menor(self):
+
+        #tfg con menos de 1 alumno
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.otro_titulo_tfg,
+                   n_alumnos=-1, descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
+
+        result = tfg_services.insert_tfg(tfg)
+        self.assertEqual(result, False)
+
+    def test_tfg_n_alumnos_mayor(self):
+
+        #tfg con mas de 3 alumnos
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.otro_titulo_tfg,
+                   n_alumnos=4, descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
+
         result = tfg_services.insert_tfg(tfg)
         self.assertEqual(result, False)
 
     def test_tfg_descripcion(self):
-        tfg = Tfg()
+
+        #tfg sin descripcion
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.otro_titulo_tfg,
+                   n_alumnos=self.n_alumnos_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
+
         result = tfg_services.insert_tfg(tfg)
-        self.assertEqual(True,True)
+        self.assertEqual(result, False)
 
     def test_tfg_conocimientos_previos(self):
-        tfg = Tfg()
+
+        #tfg sin conocimientos previos
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.otro_titulo_tfg,
+                   n_alumnos=self.n_alumnos_tfg, descripcion=self.descripcion_tfg,
+                   #conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg, tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
         result = tfg_services.insert_tfg(tfg)
-        self.assertEqual(True,True)
+        self.assertEqual(result, False)
 
     def test_tfg_hard_soft(self):
-        tfg = Tfg()
+
+        #tfg sin requisitos hardware/software
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.otro_titulo_tfg,
+                   n_alumnos=self.n_alumnos_tfg, descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   #hard_soft=self.hard_soft_tfg,
+                   tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
         result = tfg_services.insert_tfg(tfg)
-        self.assertEqual(True,True)
+        self.assertEqual(result, False)
 
     def test_tfg_tutor(self):
-        tfg = Tfg()
+
+        #tfg sin tutor
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.otro_titulo_tfg,
+                   n_alumnos=self.n_alumnos_tfg, descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg,
+                   #tutor=self.user_tutor_tfg,
+                   cotutor=self.user_cotutor_tfg)
         result = tfg_services.insert_tfg(tfg)
-        self.assertEqual(True,True)
+        self.assertEqual(result, False)
 
     def test_tfg_cotutor(self):
-        tfg = Tfg()
+
+        #tfg sin cotutor
+        tfg = Tfg(tipo=self.tipo_tfg, titulo=self.otro_titulo_tfg,
+                   n_alumnos=self.n_alumnos_tfg, descripcion=self.descripcion_tfg,
+                   conocimientos_previos=self.conocimientos_previos_tfg,
+                   hard_soft=self.hard_soft_tfg,
+                   tutor=self.user_tutor_tfg,
+                   #cotutor=self.user_cotutor_tfg
+                   )
         result = tfg_services.insert_tfg(tfg)
-        self.assertEqual(True,True)
+        self.assertEqual(result, False)
