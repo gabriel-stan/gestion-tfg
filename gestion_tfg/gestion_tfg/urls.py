7f132a1337911567cde3c70fc36cdef1844ec9ef
Gabriel Stan <gabriel.stan.90@gmail.com>
2015-11-11 23:31:45 +0100

    modificando tests

--- gestion_tfg/gestion_tfg/urls.py
+++ gestion_tfg/gestion_tfg/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-import views
-
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index, name='index'),
 ]
