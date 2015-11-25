9608baf39ea23658fb2ca1382843c8290822918f
Gabriel Stan <gabriel.stan.90@gmail.com>
2015-11-26 00:06:48 +0100

    fixing static

--- gestion_tfg/gestion_tfg/settings.py
+++ gestion_tfg/gestion_tfg/settings.py
@@ -96,15 +96,16 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+# Static config for deployment to Heroku
+
+STATIC_ROOT = 'staticfiles'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Static config for deployment to Heroku
-
-#STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
