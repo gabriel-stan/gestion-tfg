00daa58660c26cb84714b098d7fef99a0ad92a21
Gabriel Stan <gabriel.stan.90@gmail.com>
2015-11-27 18:29:03 +0100

    jugnado con travis

--- auto-merge.sh
+++ auto-merge.sh
@@ -6,4 +6,17 @@
 
 echo $TRAVIS_BRANCH
 
+echo $MIVAR
+
+if [[ $TRAVIS_BRANCH =~ ^B ]]; then
+
+	#export BRANCHES_TO_MERGE_REGEX="$TRAVIS_BRANCH"
+	#export BRANCH_TO_MERGE_INTO=dev
+	#export GITHUB_REPO=gabriel-stan/gestion-tfg
+
+	#./travis-auto-merge.sh
+else
+	echo "no no"
+fi
+
 #BRANCHES_TO_MERGE_REGEX='^f/' BRANCH_TO_MERGE_INTO=develop GITHUB_REPO=cdown/srt /tmp/travis-automerge
