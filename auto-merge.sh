8eeae0daae700177881e158d1e077d4b0f6debfc
Gabriel Stan <gabriel.stan.90@gmail.com>
2015-11-28 19:43:47 +0100

    comprobando variables de entorno de travis travis_message y no sincronizo

--- auto-merge.sh
+++ auto-merge.sh
@@ -4,34 +4,42 @@
 #chmod a+x /tmp/travis-automerge
 
 COMMIT_MESSAGE="$(git log -1 --pretty=%B)"
-SYNC_DEV="*SYNC_DEV*"
+SYNC_DEV="*(SYNC_DEV|SYNC_MASTER)*"
+SYNC_MASTER="*SYNC_MASTER*"
 
 echo $TRAVIS_BRANCH
 
 if [[ $TRAVIS_BRANCH =~ ^B ]]; then
 
-	echo $COMMIT_MESSAGE
-
 	if [[ $COMMIT_MESSAGE == $SYNC_DEV ]]; then
 
-		echo "Sincronizando con dev..."
+		echo "Sincronizando con DEV..."
 
 		export BRANCHES_TO_MERGE_REGEX="$TRAVIS_BRANCH"
 		export BRANCH_TO_MERGE_INTO=dev
 		export GITHUB_REPO=gabriel-stan/gestion-tfg
 
 		./travis-auto-merge.sh
+
 	else
-		echo "No se sincroniza con dev"
+		echo "No se sincroniza con DEV"
 	fi
 
 elif [[ $TRAVIS_BRANCH =~ ^dev ]]; then
 
-	export BRANCHES_TO_MERGE_REGEX="$TRAVIS_BRANCH"
-	export BRANCH_TO_MERGE_INTO=master
-	export GITHUB_REPO=gabriel-stan/gestion-tfg
+	if [[ $COMMIT_MESSAGE == $SYNC_MASTER ]]; then
 
-	./travis-auto-merge.sh
+		echo "Sincronizando con MASTER..."
+
+		export BRANCHES_TO_MERGE_REGEX="$TRAVIS_BRANCH"
+		export BRANCH_TO_MERGE_INTO=master
+		export GITHUB_REPO=gabriel-stan/gestion-tfg
+
+		./travis-auto-merge.sh
+
+	else
+		echo "No se sincroniza con MASTER"
+	fi
 fi
 
 #BRANCHES_TO_MERGE_REGEX='^f/' BRANCH_TO_MERGE_INTO=develop GITHUB_REPO=cdown/srt /tmp/travis-automerge
