ac30256c1359c5c70e9a2c7248dc27cc8ae9a283
Gabriel Stan <gabriel.stan.90@gmail.com>
2015-11-28 18:25:56 +0100

    no me desaparezcas...

--- auto-merge.sh
+++ auto-merge.sh
@@ -13,6 +13,13 @@ if [[ $TRAVIS_BRANCH =~ ^B ]]; then
 	export GITHUB_REPO=gabriel-stan/gestion-tfg
 
 	./travis-auto-merge.sh
+elif [[ $TRAVIS_BRANCH =~ ^dev ]]; then
+
+	export BRANCHES_TO_MERGE_REGEX="$TRAVIS_BRANCH"
+	export BRANCH_TO_MERGE_INTO=master
+	export GITHUB_REPO=gabriel-stan/gestion-tfg
+
+	./travis-auto-merge.sh
 fi
 
 #BRANCHES_TO_MERGE_REGEX='^f/' BRANCH_TO_MERGE_INTO=develop GITHUB_REPO=cdown/srt /tmp/travis-automerge
