81dda4d147a2cc4a81bd79002f78b09507b0736c
Gabriel Stan <gabriel.stan.90@gmail.com>
2015-11-28 23:41:28 +0100

    moviendo definitivamente despliegue a master SYNC_MASTER

--- gestion_tfg/gestion_tfg/__init__.py
+++ gestion_tfg/gestion_tfg/__init__.py
@@ -1,4 +1,4 @@
 #import signals
 
-#cargar las señañes cuando la app este lista. ver apps.py
+#cargar las senales cuando la app este lista. ver apps.py
 default_app_config = 'gestion_tfg.apps.GestionTFGConfig'
