1f7f9379ab4488029d98ddf28db176ab72e2a932
Gabriel Stan <gabriel.stan.90@gmail.com>
2016-01-28 23:36:58 +0100

    added make migrations to install SYNC_MASTER

--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ sudo apt-get install -y python-dev libffi-dev libssl-dev curl
 
 pip install virtualenv
 
+
 virtualenv venv
 source venv/bin/activate
 
+
 pip install -r requirements.txt
+
 
 python manage.py syncdb --noinput
 python manage.py migrate
