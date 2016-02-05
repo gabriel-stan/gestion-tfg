f32e79760f543e6412e3b3943f31e1c0a64f5980
Gabriel Stan <gabriel.stan.90@gmail.com>
2016-02-06 00:42:36 +0100

    create superuser automatically on deployment

--- install.sh
+++ install.sh
@@ -11,8 +11,11 @@ pip install -r requirements.txt
 cd gestion_tfg
 
 python manage.py syncdb --noinput
+
+echo "from django.contrib.autport get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+
 python manage.py migrate
-python manage.py migrate
+#python manage.py migrate
 
 deactivate
 
