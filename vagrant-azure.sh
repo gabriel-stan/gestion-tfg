73135fdea46b2af4f2695b69a1825685b26e3374
Gabriel Stan <gabriel.stan.90@gmail.com>
2016-02-08 17:07:03 +0100

    ultimos retoques... SYNC_MASTER

--- vagrant-azure.sh
+++ vagrant-azure.sh
@@ -7,7 +7,20 @@ ENV_VARS=~/azure/env_vars.sh
 $ENV_VARS
 
 if [[ $# == 1 ]]; then
-	vagrant $1
+
+	if [[ $1 == 'norun' ]]; then
+
+		#head -n -1 .env > .env.tmp ; mv .env.tmp .env
+
+		echo SKIP_TAGS="--skip-tags=runserver" >> .env
+		
+		vagrant up --provider=azure
+
+		#$FABRIC="fa -p '$VM_PASS' -H '$VM_USER'@'$VM_NAME'.cloudapp.net run_app"
+
+	else
+		vagrant $1
+	fi
 else
 	vagrant up --provider=azure
 fi
