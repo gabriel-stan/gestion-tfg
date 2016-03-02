d770e2547bd5091c4c9e595119659bca8887a9cd
Gabriel Stan <gabriel.stan.90@gmail.com>
2016-03-02 21:16:01 +0100

    fixing scripts

--- utils/scripts/createsuperuser.py
+++ utils/scripts/createsuperuser.py
@@ -1,3 +1,10 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestfg.settings")
 from django.contrib.auth import get_user_model
 User = get_user_model()
-User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+
+try:
+    auth_models.User.objects.get(username='admin')
+except auth_models.User.DoesNotExist:
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
