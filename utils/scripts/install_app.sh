b04a17d6b65b7955893f09d3247c56ea54a4bd48
Gabriel Stan <gabriel.stan.90@gmail.com>
2016-04-03 18:41:25 +0200

    added front scripts

--- utils/scripts/install_app.sh
+++ utils/scripts/install_app.sh
@@ -7,15 +7,31 @@
 virtualenv venv
 source venv/bin/activate
 
-# get or set requirements file
+# get or set back requirements file
 REQUIREMENTS_BACK=$1
 
 if [ ! -f $REQUIREMENTS_BACK ]; then
     REQUIREMENTS_BACK=utils/requirements_back.txt
 fi
 
+# get or set front requirements file
+REQUIREMENTS_FRONT=$2
+
+if [ ! -f $REQUIREMENTS_FRONT ]; then
+    REQUIREMENTS_FRONT=utils/requirements_front.txt
+fi
+
 # install pip requirements
-pip install -r $1
+pip install -r $REQUIREMENTS_BACK
+pip install -r $REQUIREMENTS_FRONT
+
+# install front requirements
+nodeenv --node=4.4.0 --prebuilt venvnode
+source venvnode/bin/activate
+
+npm install -g bower
+npm install
+bower install
 
 # prepare database
 python manage.py syncdb --noinput
