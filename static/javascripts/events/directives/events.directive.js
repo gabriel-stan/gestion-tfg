b71b77bea4ab75df2a7b6f4f15fc74dd15003b50
Gabriel Stan <gabriel.stan.90@gmail.com>
2016-04-24 20:37:50 +0200

    directiva para eventos

---
+++ static/javascripts/events/directives/events.directive.js
@@ -0,0 +1,33 @@
+/**
+* Events
+* @namespace gestfg.events.directives
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('gestfg.events..directives')
+    .directive('events', events);
+
+  /**
+  * @namespace Events
+  */
+  function events() {
+    /**
+    * @name directive
+    * @desc The directive to be returned
+    * @memberOf gestfg.events.directives.Events
+    */
+    var directive = {
+      controller: 'EventsController',
+      controllerAs: 'vm',
+      restrict: 'E',
+      scope: {
+        posts: '='
+      },
+      templateUrl: '/static/templates/events/events.html'
+    };
+
+    return directive;
+  }
+})();
