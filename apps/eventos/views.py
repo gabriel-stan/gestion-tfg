750b5f0133a56edb04782a4459213416b3cfe2a8
tonimademo <tonimademo@gmail.com>
2016-04-29 00:26:26 +0200

    BACKEND-2 SYN_DEV ajuste eventos

--- apps/eventos/views.py
+++ apps/eventos/views.py
@@ -2,6 +2,7 @@
 from eventos.models import Evento
 from eventos.serializers import EventoSerializer
 from authentication.models import Usuario, Profesor
+from authentication.serializers import UsuarioSerializer
 from rest_framework import viewsets, status, views
 from rest_framework.response import Response
 import json
@@ -23,7 +24,7 @@ class EventosViewSet(viewsets.ModelViewSet):
 
         """
         try:
-            eventos = Evento.objects.filter(autor=request.user.alumno.id)
+            eventos = Evento.objects.filter(autor=request.user.id)
             resul = self.serializer_class(eventos, many=True).data
             return Response(dict(status=True, data=resul))
         except NameError as e:
@@ -42,14 +43,9 @@ class EventosViewSet(viewsets.ModelViewSet):
         try:
             content = request.data['content']
             tipo = 'info'
-            serializer = self.serializer_class(data={'contenido': content, 'tipo': tipo, 'autor': request.user.id})
-            if serializer.is_valid():
-                resul = serializer.create_evento(serializer.validated_data)
-                if resul['status']:
-                    return Response(utils.to_dict(resul))
-                else:
-                    return Response(resul)
-            else:
-                return Response(dict(status=False, message=serializer.errors), status=status.HTTP_200_OK)
+            resul = Evento.objects.create_evento(contenido=content, tipo=tipo, autor=Usuario.objects.get(id=request.user.id))
+
+            return Response(utils.to_dict(resul))
+
         except Exception as e:
             return Response(dict(status=False, message="Error en la llamada"), status=status.HTTP_400_BAD_REQUEST)
