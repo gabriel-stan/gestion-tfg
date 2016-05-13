7343f3395b7c38db4d4e86562c2ecf405b235363
tonimademo <tonimademo@gmail.com>
2016-05-13 20:56:29 +0200

    BACKEND-2 postgres

---
+++ utils/scripts/postgres.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install postgresql-9.4
+sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
+sudo -u postgres psql -U postgres -d postgres -c "ALTER ROLE postgres SET client_encoding TO 'utf8';"
+sudo -u postgres psql -U postgres -d postgres -c "ALTER ROLE postgres SET default_transaction_isolation TO 'read committed';"
+sudo -u postgres psql -U postgres -d postgres -c "ALTER ROLE postgres SET timezone TO 'UTC';"
+sudo -u postgres psql -U postgres -d postgres -c "CREATE DATABASE gestfg"
+sudo -u postgres psql -U postgres -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE gestfg TO postgres;"
