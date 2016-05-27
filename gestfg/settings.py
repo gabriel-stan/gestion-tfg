776f9ea4f51a7db9fe60c1744a1e00b6b471248b
Tonima <tonimademo@gmail.com>
2016-05-27 16:46:45 +0200

    BACKEND-2 SYNC_DEV quitando postgres

--- gestfg/settings.py
+++ gestfg/settings.py
@@ -105,23 +105,23 @@ WSGI_APPLICATION = 'gestfg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('GTFG_DB') or 'gestfg',
-        'USER': os.environ.get('GTFG_USER_DB') or 'postgres',
-        'PASSWORD': os.environ.get('GTFG_PASS_DB') or 'postgres',
-        'HOST': os.environ.get('GTFG_HOST_DB') or 'localhost',
-        'PORT': os.environ.get('GTFG_PORT_DB') or '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ.get('GTFG_DB') or 'gestfg',
+#         'USER': os.environ.get('GTFG_USER_DB') or 'postgres',
+#         'PASSWORD': os.environ.get('GTFG_PASS_DB') or 'postgres',
+#         'HOST': os.environ.get('GTFG_HOST_DB') or 'localhost',
+#         'PORT': os.environ.get('GTFG_PORT_DB') or '5432',
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
