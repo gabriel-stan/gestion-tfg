32c4ec98abbbfe4ffac4dc12be9ada59dce897be
Gabriel Stan <gabriel.stan.90@gmail.com>
2016-05-27 21:43:44 +0200

    fxing travis and postgres SYNC_MASTER

--- utils/scripts/postgres.sh
+++ utils/scripts/postgres.sh
@@ -17,7 +17,6 @@ SCRIPTS=utils/scripts
 if [[ $TRAVIS != 'true' ]]; then
 	echo "No estoy en travis"
   sudo apt-get install -y postgresql-9.4
-  sudo -u postgres psql -U postgres -d postgres -c "CREATE DATABASE $PGDATABASE;"
 else
 	echo "No instalo postgres"
 fi
@@ -27,5 +26,6 @@ sudo -u postgres psql -U postgres -d postgres -c "CREATE ROLE $PGUSER;"
 sudo -u postgres psql -U postgres -d postgres -c "ALTER ROLE $PGUSER SET client_encoding TO 'utf8';"
 sudo -u postgres psql -U postgres -d postgres -c "ALTER ROLE $PGUSER SET default_transaction_isolation TO 'read committed';"
 sudo -u postgres psql -U postgres -d postgres -c "ALTER ROLE $PGUSER SET timezone TO 'UTC';"
+sudo -u postgres psql -U postgres -d postgres -c "CREATE DATABASE $PGDATABASE;"
 # sudo -u postgres psql -U postgres -d postgres --set=nombredb="$PGDATABASE" -f $SCRIPTS/create_database.sql
 sudo -u postgres psql -U postgres -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE $PGDATABASE TO $PGUSER;"
