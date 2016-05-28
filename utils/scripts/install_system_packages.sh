94c902d0e338285af915760056a65d62669102a8
Gabriel Stan <gabriel.stan.90@gmail.com>
2016-05-28 17:40:00 +0200

    adding fabric SYNC_DEV

--- utils/scripts/install_system_packages.sh
+++ utils/scripts/install_system_packages.sh
@@ -9,4 +9,5 @@ apt-get install -y python python-dev python-setuptools python-pip
 apt-get install -y libffi-dev libssl-dev libpq-dev
 pip install virtualenv
 
+# for docker containers
 apt-get install -y sudo
