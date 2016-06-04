16f94e520032a1c2ccc65f81eaf95bb0bf71da02
Gabriel Stan <gabriel.stan.90@gmail.com>
2016-06-04 19:16:17 +0200

    Anadiendo usuarios front: controlador, directive y plantilla. Anadiendo  DataTables + internacionalizacion Spanish

---
+++ static/javascripts/users/controllers/users.controller.js
@@ -0,0 +1,37 @@
+/**
+* UsersController
+* @namespace gestfg.users.controllers
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('gestfg.users.controllers')
+    .controller('UsersController', UsersController);
+
+  UsersController.$inject = ['$scope'];
+
+  /**
+  * @namespace UsersController
+  */
+  function UsersController($scope) {
+
+    var usersCtrl = this;
+    usersCtrl.loadUsers = loadUsers;
+
+    /**
+    * @name loadUsers
+    * @desc Loads all users from database
+    * @memberOf gestfg.users.controllers.UsersController
+    */
+    function loadUsers(){
+      // alert("Load users controlador");      
+    }
+
+    // $scope.loadUsers = function() {
+    //   alert("Cargando usuarios scope");
+    // }
+
+
+  }
+})();
---
+++ static/javascripts/users/users.module.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('gestfg.users', [
+      'gestfg.users.controllers',
+      'gestfg.users.directives'
+    ]);
+
+  angular
+    .module('gestfg.users.controllers', []);
+
+  angular
+    .module('gestfg.users.directives', []);
+
+})();
