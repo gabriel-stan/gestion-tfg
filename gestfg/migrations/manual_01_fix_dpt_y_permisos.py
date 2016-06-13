f52cbfeaeaf4b96de9c6e6ab3906756ae4804cc9
Tonima <tonimademo@gmail.com>
2016-06-13 19:00:52 +0200

    SYNC_DEV arreglo migrate departamento

--- gestfg/migrations/manual_01_fix_dpt_y_permisos.py
+++ gestfg/migrations/manual_01_fix_dpt_y_permisos.py
@@ -8,17 +8,6 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     operations = [
-        migrations.RemoveField(
-            model_name='profesor',
-            name='departamento'
-        ),
-
-        migrations.AddField(
-            model_name='profesor',
-            name='departamento',
-            field=models.ForeignKey(related_name='departamento', default=None, to=settings.AUTH_USER_MODEL, null=True),
-        ),
-
         migrations.RunSQL("DELETE FROM auth_group_permissions"),
         migrations.RunSQL("DELETE FROM auth_permission WHERE codename='tfg.create' OR codename='evento.create'")
     ]
