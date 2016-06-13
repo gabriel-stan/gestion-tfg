a6cdd4876d25e8ab34c7fd702affacf4a7f6f3fb
Tonima <tonimademo@gmail.com>
2016-06-13 19:54:33 +0200

    BACKEND-2 SYNC_MASTER arreglando departamentos

--- utils/scripts/one_time_scripts/fix_departamentos_permisos.sh
+++ utils/scripts/one_time_scripts/fix_departamentos_permisos.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-# # set or get run environment variables
-ENV_VARS=$1
-
-if [ ! -f $ENV_VARS ]; then
-    ENV_VARS=utils/environment/install_env
-fi
-
-source $ENV_VARS
-export $(cut -d= -f1 "$ENV_VARS")
-
-sudo -u postgres psql -U $PGUSER -d $PGDATABASE -c "DELETE FROM auth_group_permissions;"
-sudo -u postgres psql -U $PGUSER -d $PGDATABASE -c "DELETE FROM auth_permission WHERE codename='tfg.create' OR codename='evento.create';"
+sudo -u postgres psql -U postgres -d gestfg -c "DELETE FROM auth_group_permissions;"
+sudo -u postgres psql -U postgres -d gestfg -c "DELETE FROM auth_permission WHERE codename='tfg.create' OR codename='evento.create';"
