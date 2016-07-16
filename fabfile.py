8e1f678de046557333c4b37f7419cae7943bd1b7
Gabriel Stan <gabriel.stan.90@gmail.com>
2016-07-16 14:11:47 +0200

    update fabfile SYNC_DEV

--- fabfile.py
+++ fabfile.py
@@ -23,3 +23,8 @@ def update_sources():
 def update_app():
     update_sources()
     run('cd gestion-tfg && make update_app')
+
+# restart server
+def restart_server():
+    run('cd gestion-tfg && make stop_gunicorn')
+    run('cd gestion-tfg && make run_gunicorn')
