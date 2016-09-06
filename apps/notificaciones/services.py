69b3773c1be25e4936fd6164dfbf7123316973db
tonimademo <tonimademo@gmail.com>
2016-09-06 21:04:03 +0200

    BACKEND-2 SYNC_MASTER ajustes

--- apps/notificaciones/services.py
+++ apps/notificaciones/services.py
@@ -2,8 +2,9 @@
 from django.core.mail import send_mail
 from gestfg.settings import EMAIL_HOST_USER
 import const_mensajes
+from gestfg.tasks import send_mail_task
 
 
 def email_asig_tfg(tfg, usuarios):
     a =const_mensajes.MENSAJE_ASIG_TFG.format(titulo=tfg)
-    send_mail(u'[GESTFG] - Notificación de Asignación', const_mensajes.MENSAJE_ASIG_TFG.format(titulo=tfg), EMAIL_HOST_USER, usuarios)
+    send_mail_task.delay(u'[GESTFG] - Notificación de Asignación', const_mensajes.MENSAJE_ASIG_TFG.format(titulo=tfg), EMAIL_HOST_USER, usuarios)
