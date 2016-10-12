657d3d925643c6e8471d9113e4e8068b97725922
tonimademo <tonimademo@gmail.com>
2016-10-12 18:53:10 +0200

    BACKEND-2 interfaz django rest deshabilitada

--- gestfg/settings.py
+++ gestfg/settings.py
@@ -89,6 +89,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
     )
 }
 
